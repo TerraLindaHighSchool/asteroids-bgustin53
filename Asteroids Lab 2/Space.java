da9c64b13dd119e0cf38e26cb3b78c986a1c1647
Bruce Gustin <bgustin@srcs.org>
2020-02-08 11:26:13 -0800

    Made copy of Lab 1 renaming it Lab 2

---
+++ Asteroids Lab 2/Space.java
@@ -0,0 +1,71 @@
+import greenfoot.*;
+
+/**
+ * Space. Something for rockets to fly in.
+ * 
+ * @author Michael Kölling
+ * @version 1.1
+ */
+public class Space extends World
+{
+    private GreenfootImage background = getBackground();
+    private Counter scoreCounter;
+    private int startAsteroids = 3;
+    private final int NUM_OF_STARS = 400;
+
+    /**
+     * Create the space and all objects within it.
+     */
+    public Space() 
+    {
+        super(600, 500, 1);
+        background.setColor(Color.BLACK);
+        background.fill();
+        
+        Rocket rocket = new Rocket();
+        addObject(rocket, getWidth()/2 + 100, getHeight()/2);
+        
+        addAsteroids(startAsteroids);
+        paintStars(NUM_OF_STARS);
+        
+        scoreCounter = new Counter("Score: ");
+        addObject(scoreCounter, 60, 480);
+
+        Explosion.initializeImages();
+        ProtonWave.initializeImages();
+    }
+    
+    /**
+     * Add a given number of asteroids to our world. Asteroids are only added into
+     * the left half of the world.
+     */
+    private void addAsteroids(int count) 
+    {
+        for(int i = 0; i < count; i++) 
+        {
+            int x = Greenfoot.getRandomNumber(getWidth()/2);
+            int y = Greenfoot.getRandomNumber(getHeight()/2);
+            addObject(new Asteroid(), x, y);
+        }
+    }
+    
+    private void paintStars(int count) 
+    {
+        for(int i = 0; i < count; i++) 
+        {
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            background.setColor(Color.WHITE);
+            background.fillOval(x, y, 2, 2);
+        }
+    }
+    
+    /**
+     * This method is called when the game is over to display the final score.
+     */
+    public void gameOver() 
+    {
+        // TODO: show the score board here. Currently missing.
+    }
+
+}
